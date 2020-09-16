87122b129efa938a45319bae1a1a1dca404e3f19
Getman <getman.darya@mail.ru>
2020-09-16 22:14:44 +0400

    Added getters and setters.

--- practice/src/main/java/ru/ssau/tk/_emptiness_/_first/Person.java
+++ practice/src/main/java/ru/ssau/tk/_emptiness_/_first/Person.java
@@ -15,4 +15,29 @@ public class Person {
     public Person(int passportId) {
         this.passportId = passportId;
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getPassportId() {
+        return passportId;
+    }
+
+    public void setPassportId(int passportId) {
+        this.passportId = passportId;
+    }
+
 }
